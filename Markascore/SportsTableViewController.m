2bf1553ef0f12c65e84a430103a153f8d42bfbc4
Christopher Stoll <stollcri@gmail.com>
2016-01-30 22:11:20 -0500

    Change sport sort order

--- Markascore/SportsTableViewController.m
+++ Markascore/SportsTableViewController.m
@@ -416,7 +416,7 @@
     [fetchRequest setFetchBatchSize:20];
     
     // Edit the sort key as appropriate.
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:NO];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
     NSArray *sortDescriptors = @[sortDescriptor];
     
     [fetchRequest setSortDescriptors:sortDescriptors];
